205fc9374aad1b6d76aabcaf1903d48217e229fc
Mathieu St-Louis <stlouis.mathieu@gmail.com>
2021-01-10 23:32:10 -0800

    - Added the ITimer interface which will be returned from the .Register call and leveraged by users. - Polish the ITimerFactory interface to include a generic typed Timer which will return a timer of the given type. - Move interfaces and associated implementations into their own directories.

---
+++ src/Core/Timer/ITimer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Chronos.Timer.Core
+{
+    public interface ITimer
+    {
+        public TimeSpan ElapsedTime { get; }
+        public TimeSpan TimeLeft { get; }
+        internal Guid Id { get; }
+
+        /// <summary>
+        /// Starts the timer.
+        /// </summary>
+        public void Start();
+
+        /// <summary>
+        /// Stops the timer.
+        /// </summary>
+        public void Stop();
+
+        /// <summary>
+        /// Returns true if the tracker is not running.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsStopped();
+
+        /// <summary>
+        /// Wipes the elapsed time stored in the tracker. Does not stop the timer.
+        /// </summary>
+        public void Clear();
+
+        internal void Initialize(ITimeTrackingStrategy timeTrackingStrategy, ITimerTask timerTask);
+
+        /// <summary>
+        /// Updates the elapsed time stored in the timer.
+        /// </summary>
+        internal void Update();
+    }
+}
