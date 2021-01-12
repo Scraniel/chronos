f02fc352b999833ebf25c63b23c0f38cc9ab4fc6
Mathieu St-Louis <stlouis.mathieu@gmail.com>
2021-01-12 10:26:52 -0800

    Add comments to ITimer interface

--- src/Core/Timer/ITimer.cs
+++ src/Core/Timer/ITimer.cs
@@ -1,22 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Chronos.Timer.Core
 {
+    /// <summary>
+    /// A timer which runs in the Chronos timer runtime.
+    /// </summary>
     public interface ITimer
     {
+        /// <summary>
+        /// The elapsed time since the timer was initially started.
+        /// </summary>
         public TimeSpan ElapsedTime { get; }
+
+        /// <summary>
+        /// The time left before the execution of the next action.
+        /// </summary>
         public TimeSpan TimeLeft { get; }
+
+        /// <summary>
+        /// The Timer's UID used to register/unregister with the timer runtime.
+        /// </summary>
         internal Guid Id { get; }
 
         /// <summary>
-        /// Starts the timer.
+        /// Unpauses the timer and restarts tracking time.
         /// </summary>
-        public void Start();
+        public void Unpause();
 
         /// <summary>
-        /// Stops the timer.
+        /// Pauses the timer and stops tracking time.
         /// </summary>
-        public void Stop();
+        public void Pause();
 
         /// <summary>
         /// Returns true if the tracker is not running.
@@ -25,11 +40,16 @@ namespace Chronos.Timer.Core
         public bool IsStopped();
 
         /// <summary>
-        /// Wipes the elapsed time stored in the tracker. Does not stop the timer.
+        /// Wipes the elapsed time stored in the tracker. Does not pause the timer.
         /// </summary>
         public void Clear();
 
-        internal void Initialize(ITimeTrackingStrategy timeTrackingStrategy, ITimerTask timerTask);
+        /// <summary>
+        /// Initializes the timer with the given time tracking strategy and its associated tasks.
+        /// </summary>
+        /// <param name="timeTrackingStrategy">The strategy which will provide the elapsed time.</param>
+        /// <param name="timerTasks">List of tasks to be executed by this timer.</param>
+        internal void Initialize(ITimeTrackingStrategy timeTrackingStrategy, List<ITimerTask> timerTasks);
 
         /// <summary>
         /// Updates the elapsed time stored in the timer.
