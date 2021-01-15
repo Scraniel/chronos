fd96cbaa8a49635b15ba701b76d0925699cfff24
Mathieu St-Louis <stlouis.mathieu@gmail.com>
2021-01-14 21:31:37 -0800

    Implement TimerFactory

---
+++ src/Core/TimerFactory/TimerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chronos.Timer.Core
+{
+    public class TimerFactory : ITimerFactory
+    {
+        public ITimer CreateTimer<TTimer>(ITimerTask task, ITimeTrackingStrategy timeTracker = null) where TTimer : ITimer
+        {
+            return CreateTimer<TTimer>(new List<ITimerTask> { task }, timeTracker);
+        }
+
+        public ITimer CreateTimer<TTimer>(List<ITimerTask> tasks, ITimeTrackingStrategy timeTracker = null) where TTimer : ITimer
+        {
+            TTimer timer = (TTimer)Activator.CreateInstance(typeof(TTimer));
+            timer.Initialize(timeTracker, tasks);
+            return timer;
+        }
+
+        public ITimer CreateTimer(ITimerTask task, ITimeTrackingStrategy timeTracker = null)
+        {
+            return CreateTimer(new List<ITimerTask> { task }, timeTracker);
+        }
+
+        public ITimer CreateTimer(List<ITimerTask> tasks, ITimeTrackingStrategy timeTracker = null)
+        {
+            ITimer timer = new BasicTimer();
+            timer.Initialize(timeTracker, tasks);
+            return timer;
+        }
+    }
+}
---
+++ tests/Core/TimerFactoryUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using Chronos.Timer.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chronos.Timer.Tests.Core
+{
+    [TestClass]
+    public class TimerFactoryUnitTests
+    {
+        private readonly TimerFactory _target;
+
+        public TimerFactoryUnitTests()
+        {
+            _target = new TimerFactory();
+        }
+
+        [TestMethod]
+        public void Execute_TimeRemaining_NoOp()
+        {
+            //_target.CreateTimer<BasicTimer>(
+        }
+
+        private class TestTimer
+        {
+        }
+    }
+}
