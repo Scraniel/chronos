65aa3d0be225c65e4cbaf786afdc455584d6ffce
Mathieu St-Louis <stlouis.mathieu@gmail.com>
2021-01-17 20:46:00 -0800

    address comments

--- src/Core/TimerFactory/TimerFactory.cs
+++ src/Core/TimerFactory/TimerFactory.cs
@@ -5,11 +5,24 @@ namespace Chronos.Timer.Core
 {
     public class TimerFactory : ITimerFactory
     {
+        /// <summary>
+        /// Creates a timer for the task using the strategy given.
+        /// </summary>
+        /// <typeparam name="TTimer">The type of timer to create.</typeparam>
+        /// <param name="timeTracker">The strategy which will provide the elapsed time.</param>
+        /// <returns>A new ITimer.</returns>
         public ITimer CreateTimer<TTimer>(ITimeTrackingStrategy timeTracker = null) where TTimer : ITimer
         {
             return CreateTimer<TTimer>(new List<ITimerTask>(), timeTracker);
         }
 
+        /// <summary>
+        /// Creates a timer for the task using the strategy given.
+        /// </summary>
+        /// <typeparam name="TTimer">The type of timer to create.</typeparam>
+        /// <param name="task">The task to perform.</param>
+        /// <param name="timeTracker">The strategy which will provide the elapsed time.</param>
+        /// <returns>A new ITimer.</returns>
         public ITimer CreateTimer<TTimer>(ITimerTask task, ITimeTrackingStrategy timeTracker = null) where TTimer : ITimer
         {
             if (task == null)
@@ -18,21 +31,16 @@ namespace Chronos.Timer.Core
             return CreateTimer<TTimer>(new List<ITimerTask> { task }, timeTracker);
         }
 
+        /// <summary>
+        /// Creates a timer for the list of tasks using the strategy given.
+        /// </summary>
+        /// <typeparam name="TTimer">The type of timer to create.</typeparam>
+        /// <param name="tasks">The list of tasks to perform.</param>
+        /// <param name="timeTracker">The strategy which will provide the elapsed time.</param>
+        /// <returns>A new ITimer.</returns>
         public ITimer CreateTimer<TTimer>(List<ITimerTask> tasks, ITimeTrackingStrategy timeTracker = null) where TTimer : ITimer
         {
             TTimer timer = (TTimer)Activator.CreateInstance(typeof(TTimer));
-            timer.Initialize(timeTracker, tasks);
-            return timer;
-        }
-
-        public ITimer CreateTimer(ITimerTask task, ITimeTrackingStrategy timeTracker = null)
-        {
-            return CreateTimer(new List<ITimerTask> { task }, timeTracker);
-        }
-
-        public ITimer CreateTimer(List<ITimerTask> tasks, ITimeTrackingStrategy timeTracker = null)
-        {
-            ITimer timer = new BasicTimer();
             timer.Initialize(timeTracker, tasks);
             return timer;
         }
