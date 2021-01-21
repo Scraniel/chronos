c06eea08c791b3fe6ce54edc0df41fed08f41ffb
Mathieu St-Louis <stlouis.mathieu@gmail.com>
2021-01-20 18:10:23 -0800

    Start implementation of TimerRuntime

--- src/Core/Runtime/TimerRuntime.cs
+++ src/Core/Runtime/TimerRuntime.cs
@@ -1,10 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Chronos.Timer.Core.Runtime
 {
-    class TimerRuntime
+    public class TimerRuntime : ITimerRuntime
     {
+
+        private List<ITimer> _timers = new List<ITimer>();
+        private bool _isPaused = false;
+        private Task _bleh;
+
+        public void Pause()
+        {
+            _isPaused = true;
+        }
+
+        public void Unpause()
+        {
+            _isPaused = false;
+        }
+
+        public ITimer Register(TimeSpan timeUntilExecution, Action actionToExecute)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITimer Register(TimeSpan timeUntilExecution, Action actionToExecute, ITimeTrackingStrategy strategy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITimer Register<T>(TimeSpan timeUntilExecution, Action actionToExecute, ITimeTrackingStrategy strategy, ITimerFactory timerFactory) where T : ITimer
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITimer Register(ITimeTrackingStrategy strategy, ITimerTask actionToExecute)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITimer Register<T>(ITimeTrackingStrategy strategy, ITimerTask actionToExecute, ITimerFactory timerFactory) where T : ITimer
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITimer Register<T>(ITimeTrackingStrategy strategy, IEnumerable<ITimerTask> actionsToExecute, ITimerFactory timerFactory) where T : ITimer
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITimer Register(ITimer timer)
+        {
+            _timers.Add(timer);
+
+            if(_bleh
+        }
+
+
+
+        public void Unregister(ITimer timer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unregister(Guid timerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Update()
+        {
+            while (true)
+            {
+
+            }
+        }
     }
 }
