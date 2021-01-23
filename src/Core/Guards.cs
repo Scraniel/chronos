c99e84c1060e07b785856757fbfc67c7468b796c
Mathieu St-Louis <stlouis.mathieu@gmail.com>
2021-01-22 16:13:26 -0800

    Add comments

--- src/Core/Guards.cs
+++ src/Core/Guards.cs
@@ -2,14 +2,17 @@
 
 namespace Chronos.Timer.Core
 {
+    /// <summary>
+    /// Helper class which ensures pre-determined contraints are met.
+    /// </summary>
     internal static class Guards
     {
         /// <summary>
         /// Ensures that a value is not null.
         /// </summary>
+        /// <param name="argument">The object which will be validated.</param>
+        /// <param name="argumentName">The name of the object.</param>
         /// <exception cref="ArgumentNullException">Passed argument is null.</exception>
-        /// <param name="argument">The </param>
-        /// <param name="argumentName"></param>
         public static void ArgumentNotNull(object argument, string argumentName)
         {
             if (argument == null)
