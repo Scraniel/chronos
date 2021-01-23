932d1979137c65857f28d7f946ed26f9cab3acfa
Mathieu St-Louis <stlouis.mathieu@gmail.com>
2021-01-22 16:10:51 -0800

    Updates

--- src/Core/Guards.cs
+++ src/Core/Guards.cs
@@ -4,6 +4,12 @@ namespace Chronos.Timer.Core
 {
     internal static class Guards
     {
+        /// <summary>
+        /// Ensures that a value is not null.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Passed argument is null.</exception>
+        /// <param name="argument">The </param>
+        /// <param name="argumentName"></param>
         public static void ArgumentNotNull(object argument, string argumentName)
         {
             if (argument == null)
